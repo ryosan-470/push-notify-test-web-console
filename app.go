d666d18dec8802010879fb31c9687767b1309093
Ryosuke Sato <rskjtwp@gmail.com>
2017-10-02 15:33:50 +0900

    Add views

--- app.go
+++ app.go
@@ -3,24 +3,40 @@ package main
 import (
 	"log"
 	"net/http"
+	"net/url"
 
 	"github.com/gorilla/mux"
+	"github.com/unrolled/render"
+)
+
+var (
+	re      *render.Render
+	baseUrl *url.URL
 )
 
 func topHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, World\n"))
+	re.HTML(w, http.StatusOK, "index", "")
 }
 
-func sendPushNotificationHandler(w http.ResponseWriter, r *http.Request) {
+func getPushNotificationHandler(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
-func listPushNotificationHandler(w http.ResponseWriter, r *http.Request) {
+func postPushNotificationHandler(w http.ResponseWriter, r *http.Request) {
 	return
 }
 
 func main() {
+	re = render.New(render.Options{
+		Directory: "views",
+	})
+
 	r := mux.NewRouter()
 	r.HandleFunc("/", topHandler)
+
+	l := r.PathPrefix("/send").Subrouter()
+	l.Methods("GET").HandlerFunc(getPushNotificationHandler)
+	l.Methods("POST").HandlerFunc(postPushNotificationHandler)
+
 	log.Fatal(http.ListenAndServe(":8080", r))
 }
